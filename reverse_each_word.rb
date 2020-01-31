21027a68dcba5848088695e52112c912bc4d4c36
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 23:49:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(str)
   arr = str.split(" ")
-  sorted = arr.each(){|word| p word.reverse}
+  sorted = arr.each(){|word| word.reverse}
+  p sorted
   return sorted.join(" ")
 end
